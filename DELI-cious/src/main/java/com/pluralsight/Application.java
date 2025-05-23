70f6d0d2d4fb0ca9f5946151468ea97387ccce48
Lesley Solorzano <lsolorzano@appdev.yearup.org>
2025-05-23 12:52:53 -0500

    Add Home screen  with UI class and basic menu loop.

---
+++ DELI-cious/src/main/java/com/pluralsight/Application.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+import com.pluralsight.UserInterface;
+public class Application {
+    public static void main(String[] args) {
+        //create the ui and show home screen
+        UserInterface ui = new UserInterface();
+        ui.showHomeScreen();
+    }
+}

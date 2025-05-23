3bac5e4d1369da6c1da94d3ca2a0749c6639a7a9
Lesley Solorzano <lsolorzano@appdev.yearup.org>
2025-05-23 12:04:25 -0500

    First commit

---
+++ DELI-cious/src/main/java/com/pluralsight/UserInterface.java
@@ -0,0 +1,10 @@
+package com.pluralsight;
+import java.util.Scanner;
+//this class handles all the user interaction through the console
+public class UserInterface {
+    private Scanner scanner;
+
+    //Constructor
+
+
+}
